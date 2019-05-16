62e6d8403135fd5c718d885304771e534c267bc6
Roman Bogorodskiy <bogorodskiy@gmail.com>
2019-05-16 18:54:26 +0400

    Chase yet another change
    All of a sudden, vodokanal decided to slugify URLS. Chase
    that in the parser code.
    
    Also, move data fetching code to functions to make testing easier.

--- vodokanal.py
+++ vodokanal.py
@@ -34,8 +34,19 @@ def get_bot(bot_token, bot_proxy):
     return bot
 
 
+def get_news_index_data():
+    """Get news index data"""
+    f = urlopen(VODO_URL)
+    return f.read().decode('utf-8')
+
+def get_news_data(news_url):
+    """Get specific news item"""
+    f = urlopen(URL_BASE + news_url)
+    return f.read().decode('utf-8')
+
+
 class VodokanalParser(HTMLParser):
-    HREF_REGEXP = "/news/operativnyy-monitoring/(?P<id>[0-9]+)/"
+    HREF_REGEXP = "/news/operativnyy-monitoring/(?P<id>[0-9a-zA-Z_-]+)/"
 
     def __init__(self, *args, **kwargs):
         HTMLParser.__init__(self, *args, **kwargs)
@@ -55,7 +66,7 @@ class VodokanalParser(HTMLParser):
             if match:
                 match_id = match.groupdict().get('id')
                 if match_id:
-                    self._news_links[int(match_id)] = href
+                    self._news_links[match_id] = href
 
 
 class VodokanalNewsDetailsParser(HTMLParser):
@@ -150,16 +161,13 @@ if __name__ == "__main__":
     c = conn.cursor()
     c.execute(
         '''CREATE TABLE IF NOT EXISTS news
-        (news_id integer PRIMARY KEY, title text, date date,
+        (news_id text PRIMARY KEY, title text, date date,
         created_at datetime, updated_at datetime, details text, news_url text, sent integer)'''
     )
     conn.commit()
 
-    f = urlopen(VODO_URL)
-    data = f.read().decode('utf-8')
-
     parser = VodokanalParser()
-    parser.feed(data)
+    parser.feed(get_news_index_data())
 
     for news_id, news_url in sorted(parser._news_links.items()):
         ret = c.execute('SELECT sent FROM news where news_id = ?',  (news_id,))
@@ -167,8 +175,7 @@ if __name__ == "__main__":
         if db_item and db_item[0] == 1:
             continue
 
-        f = urlopen(URL_BASE + news_url)
-        news_data = f.read().decode('utf-8')
+        news_data = get_news_data(news_url)
         news_item_parser = VodokanalNewsDetailsParser()
         news_item_parser.feed(news_data)
         news_item = news_item_parser.get_item()
@@ -184,9 +191,8 @@ if __name__ == "__main__":
         msg_tokens = []
         msg_tokens.append('*' + news_item.title + '*')
         msg_tokens.append(news_item.details)
-        details = news_item.details
 
-        msg_tokens.append(URL_BASE + news_url)
+        msg_tokens.append(URL_BASE + news_url.replace("_", "\\_"))
         message = u'\n'.join(msg_tokens)
 
         try:
